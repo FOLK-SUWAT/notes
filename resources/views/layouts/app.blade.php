8a47edf37bed614de155c686c588914da30acb9b
FOLKQQ <41606593+FOLKQQ@users.noreply.github.com>
2020-11-10 19:09:38 +0700

    folk
    folk

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
+<html lang="{{ app()->getLocale() }}" >
+<head >
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,8 +12,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
-<body>
+<body  style="height: 1000px;background: linear-gradient(to bottom, #0000ff -4%, #000066 100%);">
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
